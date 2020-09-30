655f4e499de296a5785042d3b13428a837ee0cb1
Diego <diegodominguez333@gmail.com>
2020-09-29 19:40:04 -0600

    state changing

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,44 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+          debugShowCheckedModeBanner: false,
+          home: Scaffold(
+            backgroundColor: Colors.blue,
+            appBar: AppBar(
+              title: Text('Ask Me Anything'),
+              backgroundColor: Colors.blue.shade900,
+            ),
+            body: BallPage(),
+          )),
+    );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballnum = 1;
+
+  void changeBallFace() {
+    setState(() {
+      ballnum = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: FlatButton(
+                  onPressed: () => changeBallFace(),
+                  child: Image.asset('images/ball$ballnum.png'))),
+        ],
       ),
     );
+  }
+}
